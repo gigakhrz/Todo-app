b6b159ed3561c91172a397d151158ec43192101c
gigakhrz <kharazishvilig13@gmail.com>
2023-06-08 15:30:34 +0400

    add in app lightmode logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 import bgLight from "./assets/bg-mobile-light.jpg";
+import bgDark from "./assets/bg-mobile-dark.jpg";
 import GlobalStyles from "./GlobalStyles";
 import Todo from "./Components/Todo";
 import Header from "./Components/Header";
+import { useSelector } from "react-redux";
+import { RootState } from "./features/store";
 
 function App() {
+  const mode = useSelector((store: RootState) => store.lightMode.dark);
+
   return (
     <>
       <GlobalStyles />
-      <Container>
+      <Container mode={mode}>
         <Header />
         <Todo />
       </Container>
@@ -18,14 +23,14 @@ function App() {
 
 export default App;
 
-const Container = styled.main`
+const Container = styled.main<{ mode: boolean }>`
   width: 100%;
   min-height: 100vh;
   display: flex;
   align-items: center;
   flex-direction: column;
   gap: 40px;
-  background-image: url(${bgLight});
+  background-image: url(${(props) => (props.mode ? bgDark : bgLight)});
   background-repeat: no-repeat;
   background-size: 100% 200px;
   padding-top: 48px;
